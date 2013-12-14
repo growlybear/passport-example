0e20375386604832b397c4dd4526e39cd2dbd8fa
Michael Allan <michaelallan@optushome.com.au>
2013-12-14 13:45:25 +1100

    Add local and remote mongodb connection strings

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    // url: 'mongodb://growlybear:bungee@dharma.mongohq.com:10084/passport-example'
+    url: 'mongodb://127.0.0.1:27017/passport-example'
+};
