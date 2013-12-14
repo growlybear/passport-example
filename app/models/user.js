c614780a9df74bae77d0970161cfc70f0293e093
Michael Allan <michaelallan@optushome.com.au>
2013-12-15 10:23:46 +1100

    Add user model

---
+++ app/models/user.js
@@ -0,0 +1,56 @@
+// app/models/user.js
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+
+    local: {
+        email   : String,
+        password: String
+    },
+
+    facebook: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+
+    twitter: {
+        id: String,
+        token: String,
+        displayName: String,
+        username: String
+    },
+
+    google: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    }
+});
+
+// methods ====================================================================
+// check if password is valid using bcrypt
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// hash and set the user's password
+// TODO salt and provide other methods for securing users' passwords
+userSchema.methods.hashPassword = function (password) {
+    var user = this;
+
+    // hash the password
+    bcrypt.hash(password, null, null, function (err, hash) {
+        if (err) return next(err);
+
+        user.local.password = hash;
+    });
+}
+
+// create the model for users and expose it to out app
+module.exports = mongoose('User', userSchema);
