61454063588e8988b1cace1ff887d6cd10e40eb2
Michael Allan <michaelallan@optushome.com.au>
2013-12-15 01:14:22 +1100

    Serve the static content from the app/components directory

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.configure(function () {
     app.use(express.methodOverride());
 
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'app', 'components')));
 
     app.set('view engine', 'ejs');
     app.set('views', path.join(__dirname, 'views'));
