afa04d5abe85cbc6115ada91834e6f7dd2ef8b51
Michael Allan <michaelallan@optushome.com.au>
2013-12-16 11:56:39 +1100

    Add local-login strategy

--- config/passport.js
+++ config/passport.js
@@ -31,6 +31,7 @@ module.exports = function (passport) {
     // ========================================================================
     // use named strategies since we have one for login and one for signup
     // by default, if there were no name, it would just be called "local"
+
     passport.use('local-signup', new LocalStrategy({
 
         // NOTE by default, local strategy uses username and password
@@ -38,11 +39,13 @@ module.exports = function (passport) {
         usernameField: 'email',
         passwordField: 'password',
         passReqToCallback: true     // pass the entire request to the callback
+
     }, function (req, email, password, done) {
 
         // find a user whose email is the same as the form's email
         // ie. see if the user already exists
         User.findOne({ 'local.email': email }, function (err, user) {
+
             if (err) return done(err);
 
             var newUser;
@@ -50,7 +53,8 @@ module.exports = function (passport) {
             // check to see if that email has already been taken
             if (user) {
                 return done(null, false, req.flash('signupMessage', 'That email is already registered'))
-            } else {
+            }
+            else {
 
                 // if there is no user with that email already, create the user
                 newUser = new User();
@@ -61,11 +65,49 @@ module.exports = function (passport) {
 
                 // save the user
                 newUser.save(function (err) {
-                    if (err) throw err;     // FIXME?
+                    if (err) throw err;     // FIXME? need better error handling
 
                     return done(null, newUser);
                 });
             }
+        });
+    }));
+
+
+    // ========================================================================
+    // LOCAL LOGIN
+    // ========================================================================
+    // use named strategies since we have one for login and one for signup
+    // by default, if there were no name, it would just be called "local"
+
+    passport.use('local-login', new LocalStrategy({
+
+        // NOTE by default, local strategy uses username and password
+        //      - we'll override that with email and password
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true     // pass the entire request to the callback
+
+    }, function (req, email, password, done) {
+
+        // find a user whose email is the same as the form's email
+        // ie. see if the user already exists
+        User.findOne({ 'local.email': email }, function (err, user) {
+
+            if (err) return done(err);
+
+            // if no user is found, return the message
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
+            }
+
+            // if the user is found, but the password is wrong
+            if (!user.validPassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Incorrect credentials.'));
+            }
+
+            // all is well, return successful user
+            return done(null, user);
         });
     }));
 };
